312cbfac510c7d0748793fd28664ca72de0ce7a9
H A <hugoa3520@gmail.com>
2020-03-18 18:10:20 +1100

    Some stuff
    Stuff

---
+++ Rollout/Assets/Move.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Move : MonoBehaviour
+{
+    private Rigidbody2D rigid2D;
+    private Vector2 input;
+    public float speed = 1;
+    public float acc = 10;
+    public float rollsize;
+
+    public LayerMask notmeiswear;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rigid2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        rigid2D.AddForce(input.x * Vector2.right * speed * acc);
+    }
+
+    void Goo()
+    {
+
+    }
+
+    void LateUpdate()
+    {
+        rigid2D.velocity = new Vector2(Mathf.Clamp(rigid2D.velocity.x, -speed, speed), rigid2D.velocity.y);
+    }
+}
+/*Junk
+ *        if (Physics2D.Raycast(transform.position, Vector2.down, 0.1f, notmeiswear))
+        {
+            groundAcc = acc;
+        }
+        else groundAcc = acc * 0.25f;
+ * 
+ */
